f427ef2b6241cd26bd3c77fcc4d7dfa94669b8ca
Khane <Sanika.Khane@Klingelnberg.com>
2025-01-06 17:39:23 +0530

    Added the straem rendering

---
+++ StudentManagementWebApp/Components/Pages/StudentOverview.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentManagementWebApp.Components.Pages
+{
+    public partial class StudentOverview
+    {
+        private List<Student> students { get; set; } = default!;
+        private int count;
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(2000);
+            students = Student.GetList();
+            count = students.Count;
+        }
+    }
+}
---
+++ StudentManagementWebApp/Student.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace StudentManagementWebApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public int Marks { get; set; }
+
+        public Student(int id, string? name, int age, int marks)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Marks = marks;
+        }
+        public static List<Student> GetList()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student(1,"Sanika",21,97));
+            students.Add(new Student(2, "abc", 22, 96));
+            students.Add(new Student(3, "abc", 78, 95));
+            students.Add(new Student(4, "abc", 32, 90));
+
+            return students;
+        }
+    }
+}
