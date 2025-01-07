3a24a7d705c77c1dd4044bdeceef530268530808
Khane <Sanika.Khane@Klingelnberg.com>
2025-01-07 17:13:19 +0530

    Added the total count at student overview page

--- StudentManagementWebApp/Components/Pages/StudentOverview.razor.cs
+++ StudentManagementWebApp/Components/Pages/StudentOverview.razor.cs
@@ -5,7 +5,7 @@ namespace StudentManagementWebApp.Components.Pages
     public partial class StudentOverview
     {
         private List<Student> students = new();
-
+        int count = StudentManager.students.Count;
         protected override void OnInitialized()
         {
             students = StudentManager.students;
